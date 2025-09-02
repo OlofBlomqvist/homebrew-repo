29151c6a3a1da39f0f036d53b121e790aa597467
Olof Blomqvist <olof@twnet.se>
2025-09-02 11:57:56 +0200

    Update odd-box.rb

--- odd-box.rb
+++ odd-box.rb
@@ -6,17 +6,17 @@ class OddBox < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/OlofBlomqvist/odd-box/releases/download/v0.1.12/odd-box-aarch64-apple-darwin"
-      sha256 "1b72b58ba3dff9187c7d546b7876db96a2c798a6278c534b8b1bf78b0ebb16ea"
+      sha256 "33fb63799f45e7ff890f434f059206fbf732fe8560d25e44c259df7e00824dcd"
     else
       url "https://github.com/OlofBlomqvist/odd-box/releases/download/v0.1.12/odd-box-x86_64-apple-darwin"
-      sha256 "21dcc595923ce81489885734d5e91c93ab0e8d37f0c9153e6c9739f846fbc1ab"
+      sha256 "20ff93f665dd193c80a05697ab2331bba14bb792c15052d343f01a855640c346"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/OlofBlomqvist/odd-box/releases/download/v0.1.12/odd-box-x86_64-unknown-linux-musl"
-      sha256 "cf706b98e6f681d22ccbf034f72ac9e256120f574899821040b282b6e6b02bfc"
+      sha256 "ebb5db8be223a265e5105ea4b35166ec0f5d5a63cdc1f3f4807417b43d771cd6"
     end
   end
 
